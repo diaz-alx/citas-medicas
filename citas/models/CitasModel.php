a01967c946eeb83b5f093c02f6b6b266112a3c28
diaz-alex <gabriel.diaz4@utp.ac.pa>
2021-07-26 23:15:50 -0500

    nuevos atributos

--- citas/models/CitasModel.php
+++ citas/models/CitasModel.php
@@ -10,7 +10,7 @@
 			$this->citas = array();
 		}
 		
-		public function get_Recetas()
+		public function get_cita()
 		{
 			$sql = "SELECT * FROM citas";
 			$resultado = $this->db->query($sql);
@@ -21,17 +21,17 @@
 			return $this->citas;
 		}
 
-		// fecha,hora,sucursal,descripcion, especialidad, doctor
-		// todo cambio que hagas en esta funcion se tiene que ver reflejado en las otras, los atributoss
-		public function insertar($nombre, $sucursal, $hora, $fecha, $especialidad, $procedimiento){
+		// usuario, fecha,hora,sucursal, especialidad, doctor, descripcion,
+		// 
+		public function insertar($usuario, $fecha, $hora, $sucursal, $especialidad, $doctor, $descripcion){
 			
-			$resultado = $this->db->query("INSERT INTO citas (nombre, calorias, hora, fecha, especialidad, procedimiento) VALUES ('$nombre', '$sucursal', '$hora', '$fecha', '$especialidad', '$procedimiento')");
+			$resultado = $this->db->query("INSERT INTO citas (usuario, fecha, hora, sucursal, especialidad, doctor, descripcion) VALUES ('$usuario', '$fecha', '$hora', '$sucursal', '$especialidad', '$doctor', '$descripcion')");
 			
 		}
 		
-		public function modificar($id, $nombre, $sucursal, $hora, $fecha, $especialidad, $procedimiento){
+		public function modificar($id, $usuario, $fecha, $hora, $sucursal, $especialidad, $doctor, $descripcion){
 			
-			$resultado = $this->db->query("UPDATE citas SET nombre='$nombre', calorias='$sucursal', hora='$hora', fecha='$fecha', especialidad='$especialidad', procedimiento='$procedimiento' WHERE id = '$id'");			
+			$resultado = $this->db->query("UPDATE citas SET usuario='$usuario', fecha='$fecha', hora='$hora', sucursal='$sucursal', especialidad='$especialidad', doctor='$doctor', descripcion='$descripcion' WHERE id = '$id'");			
 		}
 		
 		public function eliminar($id){
@@ -40,7 +40,7 @@
 			
 		}
 		
-		public function get_receta($id)
+		public function get_citas($id)
 		{
 			$sql = "SELECT * FROM citas WHERE id='$id' LIMIT 1";
 			$resultado = $this->db->query($sql);
